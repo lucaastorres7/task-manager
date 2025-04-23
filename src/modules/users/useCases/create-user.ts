f0f55b36afee9203f5be659ad99edbaab8396abc
Lucas Torres <lucastorreslr10@gmail.com>
2025-04-23 16:16:08 -0300

    feat: password encryption

--- src/modules/users/useCases/create-user.ts
+++ src/modules/users/useCases/create-user.ts
@@ -1,4 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
 import { PrismaService } from 'src/infra/database/prisma.service';
 import { CreateUserDTO } from '../dto/users.dto';
 
@@ -16,8 +17,13 @@ export class CreateUserUseCase {
       throw new HttpException('User already exists!', HttpStatus.BAD_REQUEST);
     }
 
+    const passwordHashed = await hash(data.password, 10);
+
     return await this.prisma.user.create({
-      data,
+      data: {
+        ...data,
+        password: passwordHashed,
+      },
     });
   }
 }
