6dd880e20ab1d0ab6c9f3ea9854a8c4b94257b1f
Lucas Torres <lucastorreslr10@gmail.com>
2025-04-23 15:33:05 -0300

    feat: handling user exists error

--- src/modules/users/useCases/create-user.ts
+++ src/modules/users/useCases/create-user.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/infra/database/prisma.service';
 import { CreateUserDTO } from '../dto/users.dto';
 
@@ -13,7 +13,7 @@ export class CreateUserUseCase {
     });
 
     if (user) {
-      throw new Error('User already exists!');
+      throw new HttpException('User already exists!', HttpStatus.BAD_REQUEST);
     }
 
     return await this.prisma.user.create({
